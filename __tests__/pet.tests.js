614ded7419efb63b6159babbfb871e3df5831032
Lewis Smith <lewislbssmith212@gmail.com>
2022-12-10 12:31:27 +0000

    Pet Constructor

---
+++ __tests__/pet.tests.js
@@ -0,0 +1,8 @@
+const Pet = require("../src/pet");
+
+describe("constructor", () => {
+    it("returns a new object", () => {
+        expect(new Pet("Fido")).toBeInstanceOf(Object);
+    });
+});
+
