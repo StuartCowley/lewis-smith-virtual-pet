7abc19759ec9ad60a9308f9395de39d4fdeae060
Lewis Smith <lewislbssmith212@gmail.com>
2022-12-10 13:25:22 +0000

    Prototype method added

--- __tests__/pet.tests.js
+++ __tests__/pet.tests.js
@@ -4,5 +4,19 @@ describe("constructor", () => {
     it("returns a new object", () => {
         expect(new Pet("Fido")).toBeInstanceOf(Object);
     });
+    it("set the name property", () => {
+        const pet = new Pet("Fido");
+
+        expect(pet.name).toEqual("Fido");
+    });
+    it("set initial age to 0", () => {
+        const pet = new Pet("Fido");
+        expect(pet.age).toEqual(0);
+    });
+    it("set growUp method to increase age prop by 1", () => {
+        const pet = new Pet("Fido");
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+    });
 });
 
